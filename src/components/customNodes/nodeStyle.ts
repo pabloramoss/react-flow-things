b3c4da50f9a0a52b9b418926bebef549e2024157
pabloramoss <pablo-362@hotmail.com>
2022-10-11 21:09:01 -0300

    abstract custom  node style

---
+++ src/components/customNodes/nodeStyle.ts
@@ -0,0 +1,58 @@
+export const contentStyle = {
+  contentHeader: {
+    position: "",
+    padding: "8px 0px",
+    flexGrow: 1,
+    backgroundColor: "#eee",
+  },
+  io: {
+    position: "",
+    padding: "8px 16px",
+    flexGrow: 1,
+    backgroundColor: "white",
+    borderRadius: "5px",
+    
+  },
+  left: {left: "0px"},
+  textLeft: {textAlign: "left"},
+  right: {right: "-8px"},
+  textRight: {textAlign: "right"},
+  handle: {
+    background: "#ddd",
+    borderRadius: "15px",
+    border: "2px solid #ddd",
+    boxShadow:
+      "rgba(0, 0, 0, 0.2) 0px 1px 3px 0px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 2px 1px -1px",
+  },
+};
+
+export const style = {
+  body: {
+    display: "flex",
+    flexDirection: "column",
+    transition: "all 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+    boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
+    border: "0px solid #bbb",
+    fontSize: "10pt",
+    borderRadius: "5px",
+    width: "200px",
+    textAlign: "center",
+  },
+  selected: {
+    boxShadow: "0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
+  },
+  title: {
+    position: "absolute",
+    width: "100%",
+    height: "20px",
+    padding: "2px 0px",
+    flexGrow: 1,
+    backgroundColor: "#eee",
+    borderRadius: "5px 5px 0px 0px",
+  },
+  contentWrapper: {
+    color: "black",
+    padding: "20px 0px 0px 0px",
+    borderRadius: "5px",
+  },
+};
